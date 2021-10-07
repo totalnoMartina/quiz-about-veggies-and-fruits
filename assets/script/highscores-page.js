b44431cc6d82abeed402e40289c7d0a2258b15d0
Martina Grganic <totalnobzvz@yahoo.com>
2021-10-07 23:11:40 +0000

    Add code to highscores-page.js

--- assets/script/highscores-page.js
+++ assets/script/highscores-page.js
@@ -6,7 +6,8 @@ and using JSON function we add every new score into an array, or we create a new
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 /* Reaching out to inner text of the HTML and adding a dictionary that was put inside a list 
-element with relevant class which targets connecting name of the user played and score achieved */
+element with relevant class which targets connecting name of the user played and score achieved,
+and ultimately using method to join together the two variables */
 highScoresList.innerHTML =
     highScores.map(score => {
         return `<li class="highscore">${score.name} - ${score.score}</li>`
