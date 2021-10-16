09b363f08bffb3c3bda68e73f6c1b70a838a6bd1
Martina Grganic <totalnobzvz@yahoo.com>
2021-10-16 13:08:01 +0000

    http.server

--- assets/script/high-scores.js
+++ assets/script/high-scores.js
@@ -28,14 +28,15 @@ function reset() {
 resetScore.addEventListener('click', reset);
 
 nickname.addEventListener("change", stateHandle);
-
+/*
 function buttonChange() {
     if (nickname.value === "") {
         highscoresButton.disabled = true; //button disabled if no user storing name 
     } else {
         highscoresButton.disabled = false; //button is enabled
     }
-}
+}*/
+
 // Call a buttonChange for the highscores button to show or not
-buttonChange();
+// buttonChange();
 
